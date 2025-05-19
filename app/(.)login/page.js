ce10e6565036f627f4d8f4e35425356e6a724708
Sharif-Miah <farhansharif1410@gmail.com>
2025-05-19 23:59:45 +0600

    modal code added

--- app/(.)login/page.js
+++ app/(.)login/page.js
@@ -1,8 +1,14 @@
 import Modal from '@/components/Modal';
 import React from 'react';
+import LoginPage from '../login/page';
+import LoginDetails from '@/components/auth/LoginDetails';
 
 const LoginModal = () => {
-  return <Modal />;
+  return (
+    <Modal>
+      <LoginDetails />
+    </Modal>
+  );
 };
 
 export default LoginModal;
--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,51 +1,42 @@
-import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import Link from 'next/link';
-import LoginForm from './auth/LoginForm';
+'use client';
 
-const Modal = () => {
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Link href='/login'>
-          <Button variant='outline'>Share</Button>
-        </Link>
-      </DialogTrigger>
-      <DialogContent className='sm:max-w-md'>
-        <DialogHeader>
-          <DialogTitle>Share link</DialogTitle>
-          <DialogDescription>
-            Anyone who has this link will be able to view this.
-          </DialogDescription>
-        </DialogHeader>
-        <section className='h-screen grid place-items-center bg-[#fafbfc]'>
-          <div className='max-w-[450px] w-full mx-auto p-6 border bg-[#ffffff] text-black border-gray-700/20 rounded-md'>
-            <h4 className='font-bold text-2xl'>Sign in</h4>
-            <LoginForm />
+import { createPortal } from 'react-dom';
+import { useEffect, useRef } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
-            <span className='text-center text-xs text-gray-500'>
-              Dont have an account?
-              <Link
-                className='underline hover:text-[#1455ac]'
-                href='/register'>
-                Register
-              </Link>
-            </span>
-          </div>
-        </section>
-      </DialogContent>
-    </Dialog>
+const Modal = ({ children }) => {
+  const modalRef = useRef(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!modalRef.current?.open) {
+      modalRef.current.showModal();
+    }
+  }, []);
+
+  const onHide = () => {
+    router.back();
+  };
+
+  return createPortal(
+    <dialog
+      ref={modalRef}
+      onClose={onHide}
+      className='shadow-teal-700 shadow-md border border-teal-600 flex flex-col p-2 rounded-md bg-black bg-opacity-95 text-gray-100'>
+      <span
+        onClick={onHide}
+        className='flex justify-end cursor-pointer'>
+        <Image
+          src='/xmark.svg'
+          alt='close'
+          width={30}
+          height={30}
+        />
+      </span>
+      {children}
+    </dialog>,
+    document.getElementById('modal-root-content')
   );
 };
 
