7d2130d34fae5d8e22e5d91e7ee81fe048e2219c
Sharif-Miah <farhansharif1410@gmail.com>
2025-06-29 19:57:21 +0600

    brand component style code added

--- components/Brands/BrandCart.jsx
+++ components/Brands/BrandCart.jsx
@@ -4,9 +4,9 @@ import { CarouselItem } from '../ui/carousel';
 
 const BrandCart = ({ brand }) => {
   return (
-    <CarouselItem className=' pl-1 basis-1/1 gap-4  md:basis-1/2 lg:basis-1/12'>
+    <CarouselItem className=' pl-1 basis-1/1 md:basis-1/2 lg:basis-1/12  gap-6 cursor-pointer'>
       <div className='p-1 '>
-        <Card className='w-[100px] h-[100px] rounded-full mb-6 mx-auto bg-white '>
+        <Card className='w-[70px] h-[70px] rounded-full mb-6 mx-auto bg-white '>
           <CardContent className='px-2'>
             <Image
               src={brand.image}
